8b1e5acf72237c96cabda62af0c948e2030dfb27
Gopal Menon <gopal.mnn@gmail.com>
2017-03-16 23:09:52 -0600

    Count-Min sketch seems to work, but return different results each time. Need to find out why its not deterministic.

--- HashFunctions.py
+++ HashFunctions.py
@@ -1,33 +1,36 @@
-import FileIo
 import NGrams
 import sys
-
-SALT_FILE_NAME = "Data/MobyDickChapter1.txt"
-N_GRAMS_SIZE = 3
 
 
 class NGramHash(object):
 
-    """Get character n-grams from salt file"""
-    def __get_salt_file_character_n_grams(self):
-        char_n_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(SALT_FILE_NAME), N_GRAMS_SIZE)
+    SALT_FILE_NAME = "Data/MobyDickChapter1.txt"
+    N_GRAMS_SIZE = 3
+
+    """
+    Get character n-grams from salt file
+    """
+    @staticmethod
+    def get_salt_file_character_n_grams():
+        char_n_grams = NGrams.get_character_n_grams(NGramHash.SALT_FILE_NAME, NGramHash.N_GRAMS_SIZE)
         char_n_gram_set = set()
         for n_gram in char_n_grams:
             char_n_gram_set.add(n_gram)
         return list(char_n_gram_set)
 
-    def __init__(self, hash_function_number, number_of_items_to_estimate_frequency_for):
-        self.salt_n_grams = self.__get_salt_file_character_n_grams()
+    def __init__(self, hash_function_number, number_of_items_to_estimate_frequency_for, salt_n_grams):
+        self.salt_n_grams = salt_n_grams
         self.hash_function_number = hash_function_number
         self.number_of_items_to_estimate_frequency_for = number_of_items_to_estimate_frequency_for
 
-    """Get the hash value for an n-gram. Salt for the hash
-    function is the hash_function_number n-gram in char_n_gram_set"""
-    def get_n_gram_hash(self, string_to_hash):
+    """
+    Get the hash value for a character
+    """
+    def get_stream_item_hash(self, character_to_hash):
 
         try:
             salt_for_hash_function = self.salt_n_grams[self.hash_function_number]
-            return hash((str(salt_for_hash_function) + string_to_hash)) % \
+            return hash((str(salt_for_hash_function) + character_to_hash)) % \
                    self.number_of_items_to_estimate_frequency_for
 
         except IndexError:
--- NGrams.py
+++ NGrams.py
@@ -2,18 +2,10 @@ from nltk.util import ngrams
 import nltk
 
 """Return character n-grams"""
-def get_character_n_grams(file_contents, n):
+def get_character_n_grams(file_name, n):
 
-    for file_snippet in file_contents:
-        file_n_grams = ngrams(file_snippet, n)
-        for n_gram in file_n_grams:
-            yield(n_gram)
-
-"""Return word n-grams"""
-def get_word_n_grams(file_contents, n):
-
-    for file_snippet in file_contents:
-        file_tokens = nltk.word_tokenize(file_snippet)
-        file_n_grams = ngrams(file_tokens, n)
-        for n_gram in file_n_grams:
-            yield (n_gram)
+    with open(file_name) as file_handle:
+        for file_snippet in file_handle:
+            file_n_grams = ngrams(file_snippet, n)
+            for n_gram in file_n_grams:
+                yield(n_gram)
